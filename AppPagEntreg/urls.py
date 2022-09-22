b4b5c948e6b09d237840a92c4af6c3c5af25d860
Mateo Foglia <fogliamateo@gmail.com>
2022-09-22 20:29:00 -0300

    Version_0.1

--- AppPagEntreg/urls.py
+++ AppPagEntreg/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from AppPagEntreg.views import *
 
 urlpatterns = [
-    path('', inicio),
-    path('formularioAdd/', formularioAdd),
-    path('formularioGet', formularioGet),
+    path('', inicio, name="Inicio"),
+    path('formularioAdd/', formularioAdd, name="Añadir categorías"),
+    path('formularioGet', formularioGet, name="Buscar categorías"),
 ]
